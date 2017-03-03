b2be1c27703a56ab3b8ad8f482dbba25c6503d92
Sirawit Lappisatepun <Sirawit Lappisatepun>
2017-03-03 16:25:33 +0700

    Update rulesets

--- Assets/Logic/Logic.cs
+++ Assets/Logic/Logic.cs
@@ -1,17 +1,18 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
-using Newtonsoft.Json;
+using System.Linq;
+using KMHelper;
 
 public class Logic : MonoBehaviour
 {
     public KMAudio Audio;
     public KMSelectable[] buttons;
-    public TextMesh[] info;
+    public TextMesh[] Text, sym;
+    public KMBombInfo Bomb;
 
-    public bool ansA = false, ansB = false;
-    public bool[] tog;
-    public int[] port, indc, batt, keys, num;
-    public string serl;
+    public bool[] tog, truthTable, ans = { false, false };
+    public int[] num, randomSym, paren = { 0, 0 };
+    public int indcCounts;
+    public string[] symText = { "AND", "OR" };
 
     private bool _isSolved;
 
@@ -45,104 +46,114 @@ public class Logic : MonoBehaviour
 
     void Init()
     {
+        //Generate Letters
         for (int i = 0; i < 6; i++)
         {
             num[i] = Random.Range(0, 25) + 65;
-            info[i].text = char.ConvertFromUtf32(num[i]);
+            Text[i].text = char.ConvertFromUtf32(num[i]);
         }
-        List<string> responsePORT = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_PORTS, null);
-        foreach (string response in responsePORT)
+
+        //Randomize parentheses and truth symbols
+        paren[0] = Random.Range(0, 2);
+        if(paren[0] == 0)
         {
-            Debug.Log(response);
-            Dictionary<string, string[]> responseDict = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(response);
-            foreach (string s in responseDict["presentPorts"])
-            {
-                if (string.Compare(s, "DVI", true) == 0) port[0]++;
-                else if (string.Compare(s, "Parallel", true) == 0) port[1]++;
-                else if (string.Compare(s, "PS2", true) == 0) port[2]++;
-                else if (string.Compare(s, "RJ45", true) == 0) port[3]++;
-                else if (string.Compare(s, "Serial", true) == 0) port[4]++;
-                else if (string.Compare(s, "StereoRCA", true) == 0) port[5]++;
-            }
+            Text[0].color = Color.green;
+            Text[1].color = Color.green;
         }
-        List<string> responseBATT = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_BATTERIES, null);
-        foreach (string response in responseBATT)
+        else
         {
-            Debug.Log(response);
-            Dictionary<string, int> responseDict = JsonConvert.DeserializeObject<Dictionary<string, int>>(response);
-            batt[0] += responseDict["numbatteries"];
+            Text[1].color = Color.green;
+            Text[2].color = Color.green;
         }
-        List<string> responseINDC = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_INDICATOR, null);
-        foreach (string response in responseINDC)
+
+        paren[1] = Random.Range(0, 2);
+        if (paren[1] == 0)
         {
-            Debug.Log(response);
-            Dictionary<string, string> responseDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
-            string label = responseDict["label"];
-            bool active = responseDict["on"].Equals("True");
-            if (active)
-            {
-                if (string.Compare(label, "CLR") == 0) indc[0]++;
-                else if (string.Compare(label, "FRK") == 0) indc[1]++;
-                else if (string.Compare(label, "BOB") == 0) indc[2]++;
-                else if (string.Compare(label, "CAR") == 0) indc[3]++;
-                else if (string.Compare(label, "SIG") == 0) indc[4]++;
-                else if (string.Compare(label, "MSA") == 0) indc[5]++;
-                else if (string.Compare(label, "IND") == 0) indc[6]++;
-                indc[7]++;
-            }
+            Text[3].color = Color.green;
+            Text[4].color = Color.green;
         }
-        List<string> responseSERL = GetComponent<KMBombInfo>().QueryWidgets(KMBombInfo.QUERYKEY_GET_SERIAL_NUMBER, null);
-        foreach (string response in responseSERL)
+        else
         {
-            Debug.Log(response);
-            Dictionary<string, string> responseDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(response);
-            serl = responseDict["serial"];
+            Text[4].color = Color.green;
+            Text[5].color = Color.green;
         }
-        for (int i = 0; i < 6; i++)
+
+        for (int i = 0; i < 4; i++)
         {
-            if (port[i] != 0)
-                port[6]++;
+            randomSym[i] = Random.Range(0, 2);
+            if (randomSym[i] == 0) sym[i].text = "∧";
+            else sym[i].text = "∨";
         }
         generateAns();
     }
 
     void generateAns()
     {
-        if (batt[0] > 2) keys[0] = 1;
-        if (port[4] != 0) keys[1] = 1;
-        if (port[1] != 0) keys[2] = 1;
-        for (int i = 0; i < 6; i++)
+        bool[] temp = { false, false, false, false, false, false };
+
+        //Generate truth table for all letters
+        if (Bomb.GetBatteryCount() == Bomb.GetIndicators().Count()) truthTable[0] = true;
+        if (Bomb.GetSerialNumberLetters().Count() > Bomb.GetSerialNumberNumbers().Count()) truthTable[1] = true;
+        if (Bomb.IsIndicatorPresent(KMBombInfoExtensions.KnownIndicatorLabel.IND)) truthTable[2] = true;
+        if (Bomb.IsIndicatorPresent(KMBombInfoExtensions.KnownIndicatorLabel.FRK)) truthTable[3] = true;
+        if (Bomb.GetOffIndicators().Count() == 1) truthTable[4] = true;
+        if (Bomb.GetPorts().Distinct().Count() > 1) truthTable[5] = true;
+        if (Bomb.GetBatteryCount() >= 2) truthTable[6] = true;
+        if (Bomb.GetBatteryCount() < 2) truthTable[7] = true;
+        if (Bomb.GetSerialNumberNumbers().Last() % 2 == 1) truthTable[8] = true;
+        if (Bomb.GetBatteryCount() > 4) truthTable[9] = true;
+        if (Bomb.GetOnIndicators().Count() == 1) truthTable[10] = true;
+        if (Bomb.GetIndicators().Count() > 2) truthTable[11] = true;
+        if (Bomb.GetPorts().Distinct().Count() == Bomb.GetPorts().Count()) truthTable[12] = true;
+        if (Bomb.GetBatteryHolderCount() > 2) truthTable[13] = true;
+        if (Bomb.GetOnIndicators().Count() > 0 && Bomb.GetOffIndicators().Count() > 0) truthTable[14] = true;
+        if (Bomb.IsPortPresent(KMBombInfoExtensions.KnownPortType.Parallel)) truthTable[15] = true;
+        if (Bomb.GetPorts().Count() == 2) truthTable[16] = true;
+        if (Bomb.IsPortPresent(KMBombInfoExtensions.KnownPortType.PS2)) truthTable[17] = true;
+        if (Bomb.GetSerialNumberNumbers().Sum() > 10) truthTable[18] = true;
+        if (Bomb.IsIndicatorPresent(KMBombInfoExtensions.KnownIndicatorLabel.MSA)) truthTable[19] = true;
+        if (Bomb.GetBatteryHolderCount() == 1) truthTable[20] = true;
+        if (Bomb.GetSerialNumberLetters().Any("AEIOU".Contains)) truthTable[21] = true;
+        if (Bomb.GetIndicators().Count() == 0) truthTable[22] = true;
+        if (Bomb.GetIndicators().Count() == 1) truthTable[23] = true;
+        if (Bomb.GetPorts().Count() > 5) truthTable[24] = true;
+        if (Bomb.GetPorts().Count() < 2) truthTable[25] = true;
+
+        //Check Answers for each row
+        for (int i = 0; i < 6; i++) temp[i] = truthTable[num[i] - 65];
+        Debug.LogFormat("[Logic #{0}] Truth table for symbols: {1} = {2} {3} = {4} {5} = {6}\n{7} = {8} {9} = {10} {11} = {12}",
+            _moduleId, (char)num[0], temp[0], (char)num[1], temp[1], (char)num[2], temp[2],
+            (char)num[3], temp[3], (char)num[4], temp[4], (char)num[5], temp[5]);
+
+        for(int i=0;i<2;i++)
         {
-            if (serl[i] == 'A' || serl[i] == 'E' || serl[i] == 'I' || serl[i] == 'O' || serl[i] == 'U')
-                keys[3] = 1;
+            if (paren[i] == 0)
+            {
+                if (randomSym[i * 2] == 0 && randomSym[(i * 2) + 1] == 0)
+                    if ((temp[i * 3] && temp[(i * 3) + 1]) && temp[(i * 3) + 2] == true) ans[0] = true;
+                if (randomSym[i * 2] == 0 && randomSym[(i * 2) + 1] == 1)
+                    if ((temp[i * 3] && temp[(i * 3) + 1]) || temp[(i * 3) + 2] == true) ans[0] = true;
+                if (randomSym[i * 2] == 1 && randomSym[(i * 2) + 1] == 0)
+                    if ((temp[i * 3] || temp[(i * 3) + 1]) && temp[(i * 3) + 2] == true) ans[0] = true;
+                if (randomSym[i * 2] == 1 && randomSym[(i * 2) + 1] == 1)
+                    if ((temp[i * 3] || temp[(i * 3) + 1]) || temp[(i * 3) + 2] == true) ans[0] = true;
+
+                Debug.LogFormat("[Logic #{0}] Row #{1}: ({2} {3} {4}) {5} {6} = {7}", _moduleId, i + 1, (char)num[i * 3], symText[randomSym[i * 2]], (char)num[(i * 3) + 1], symText[randomSym[(i * 2) + 1]], (char)num[(i * 3) + 2], ans[i]);
+            }
+            else
+            {
+                if (randomSym[i * 2] == 0 && randomSym[(i * 2) + 1] == 0)
+                    if (temp[i * 3] && (temp[(i * 3) + 1] && temp[(i * 3) + 2]) == true) ans[0] = true;
+                if (randomSym[i * 2] == 0 && randomSym[(i * 2) + 1] == 1)
+                    if (temp[i * 3] && (temp[(i * 3) + 1] || temp[(i * 3) + 2]) == true) ans[0] = true;
+                if (randomSym[i * 2] == 1 && randomSym[(i * 2) + 1] == 0)
+                    if (temp[i * 3] || (temp[(i * 3) + 1] && temp[(i * 3) + 2]) == true) ans[0] = true;
+                if (randomSym[i * 2] == 1 && randomSym[(i * 2) + 1] == 1)
+                    if (temp[i * 3] || (temp[(i * 3) + 1] || temp[(i * 3) + 2]) == true) ans[0] = true;
+
+                Debug.LogFormat("[Logic #{0}] Row #{1}: {2} {3} ({4} {5} {6}) = {7}", _moduleId, i + 1, (char)num[i * 3], symText[randomSym[i * 2]], (char)num[(i * 3) + 1], symText[randomSym[(i * 2) + 1]], (char)num[(i * 3) + 2], ans[i]);
+            }
         }
-        if (keys[3] == 0) keys[4] = 1;
-        if (port[5] != 0) keys[5] = 1;
-        if (indc[0] != 0) keys[6] = 1;
-        if (indc[6] != 0) keys[7] = 1;
-        if (batt[0] == 0) keys[8] = 1;
-        if (indc[5] != 0) keys[9] = 1;
-        if ((serl[5] - 48) % 2 == 0) keys[11] = 1; else keys[10] = 1;
-        if (indc[1] != 0) keys[12] = 1;
-        if (batt[0] == 1) keys[13] = 1;
-        if (batt[0] == 0) keys[14] = 1;
-        if (port[3] != 0) keys[15] = 1;
-        if (port[0] != 0) keys[16] = 1;
-        if (batt[0] > 5) keys[17] = 1;
-        if (indc[3] != 0 && indc[4] != 0) keys[18] = 1;
-        if (batt[0] > 1 && port[2] != 0) keys[19] = 1;
-        if (port[4] != 0 && port[1] != 0) keys[20] = 1;
-        if (indc[2] != 0) keys[21] = 1;
-        if (serl[0] < 65 && serl[1] < 65 && serl[2] < 65 && serl[3] < 65 && serl[4] < 65 && serl[5] < 65) keys[22] = 1;
-        if (port[6] > 3) keys[23] = 1;
-        if (indc[7] == 0) keys[24] = 1;
-        if (port[4] != 0 && port[3] != 0) keys[25] = 1;
-
-        if (keys[num[0] - 65] == 1 && keys[num[1] - 65] == 1 && keys[num[2] - 65] == 1) ansA = true;
-        if (keys[num[3] - 65] == 1 || keys[num[4] - 65] == 1 || keys[num[5] - 65] == 1) ansB = true;
-
-        Debug.LogFormat("[Logic #{0}] 1st row: {3} AND {2} AND {1} = {6} AND {5} AND {4} = {7}", _moduleId, (char) num[0], (char) num[1], (char) num[2], keys[num[0] - 65] == 1, keys[num[1] - 65] == 1, keys[num[2] - 65] == 1, ansA);
-        Debug.LogFormat("[Logic #{0}] 2nd row: {3} OR {2} OR {1} = {6} OR {5} OR {4} = {7}", _moduleId, (char) num[3], (char) num[4], (char) num[5], keys[num[3] - 65] == 1, keys[num[4] - 65] == 1, keys[num[5] - 65] == 1, ansB);
     }
 
     void HandlePress(int mode)
@@ -154,13 +165,13 @@ public class Logic : MonoBehaviour
             if (tog[mode] == false)
             {
                 buttons[mode].GetComponent<MeshRenderer>().material.color = Color.green;
-                info[mode + 6].text = "T";
+                Text[mode + 6].text = "T";
                 tog[mode] = true;
             }
             else
             {
                 buttons[mode].GetComponent<MeshRenderer>().material.color = Color.red;
-                info[mode + 6].text = "F";
+                Text[mode + 6].text = "F";
                 tog[mode] = false;
             }
         }
@@ -172,7 +183,7 @@ public class Logic : MonoBehaviour
         buttons[2].AddInteractionPunch();
         if (!_isSolved)
         {
-            if (tog[0] == ansA && tog[1] == ansB)
+            if (tog[0] == ans[0] && tog[1] == ans[1])
             {
                 Debug.LogFormat("[Logic #{0}] Module solved.", _moduleId);
                 GetComponent<KMBombModule>().HandlePass();
@@ -180,7 +191,7 @@ public class Logic : MonoBehaviour
             }
             else
             {
-                Debug.LogFormat("[Logic #{0}] 1st row: {1} (should have been {2}). 2nd row: {3} (should have been {4}).", _moduleId, tog[0], ansA, tog[1], ansB);
+                Debug.LogFormat("[Logic #{0}] 1st row: {1} (should have been {2}). 2nd row: {3} (should have been {4}).", _moduleId, tog[0], ans[0], tog[1], ans[1]);
                 GetComponent<KMBombModule>().HandleStrike();
             }
         }
